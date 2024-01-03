debb943eddf12ab43611df9ede67e7b018affca4
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-01-03 21:36:56 +0530

    data append in projects

--- src/comps/Projects/Projects.js
+++ src/comps/Projects/Projects.js
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from "react";
+import "./Projects.css";
+import { nanoid } from "nanoid";
+import Title from "../title/Title";
+import projectData from "./ProjectUtils";
 
 const Projects = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const filterList = ["All", "Websites", "Applications", "Games"];
 
-export default Projects
+  const handleFilter = (item) => {
+    console.log(item);
+  };
+  return (
+    <>
+      <Title title="Projects" />
+      <div className="Projects">
+        <ul className="filter-list">
+          {filterList.map((item) => {
+            return (
+              <li
+                key={nanoid()}
+                className="filter-item"
+                onClick={() => handleFilter(item)}
+              >
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+        <ul className="project-list">
+          {projectData.map((project) => {
+            return (
+              <li key={nanoid()} className="project-item">
+                <a
+                  href={project.previwLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <figure className="project-img">
+                    <img src={project.img} alt="" />
+                  </figure>
+                </a>
+                <h3 className="project-title">{project.title}</h3>
+                <p className="project-category">{project.category}</p>
+                <p className="project-development">{project.tech}</p>
+                <div className="project-buttons">
+                  <a
+                    href={project.previwLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <ion-icon name="eye-outline"></ion-icon>
+                    Preview
+                  </a>
+                  <a
+                    href={project.githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <ion-icon name="logo-github"></ion-icon>
+                    Github
+                  </a>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
