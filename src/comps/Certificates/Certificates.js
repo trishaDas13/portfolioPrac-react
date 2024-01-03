c916f2cadbca97b7397968bb7655cc85ac3f19d3
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-01-03 11:11:44 +0530

    certificate data added

--- src/comps/Certificates/Certificates.js
+++ src/comps/Certificates/Certificates.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import './Certificates.css'
-import {CertificatesData} from './CertificatesUtils'
+import React from "react";
+import "./Certificates.css";
+import { nanoid } from "nanoid";
+import { CertificatesData } from "./CertificatesUtils";
 
 const Certificates = () => {
-  console.log(CertificatesData);
   return (
-    <div className='certificates'>
-      
+    <div className="certificates">
+      {CertificatesData.map((data) => {
+        return (
+          <div key={nanoid()} className="certificate-box">
+            <h2 className="certificate-heading">{data.heading}</h2>
+            {data.certificates.map((certificate) => {
+              return (
+              <div key={nanoid()} className="certificate">
+                <h4>{certificate.title}</h4>
+                <p>{certificate.source}</p>
+                <a href={certificate.img} target="_blank" rel="noreferrer"><img src={certificate.img} alt="" /></a>
+              </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Certificates
+export default Certificates;
