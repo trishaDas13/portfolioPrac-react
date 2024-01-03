4727a999c6ad2d77ea9b5a502bbd74377d47e662
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-01-03 15:38:34 +0530

    certificate done

--- src/comps/Certificates/Certificates.js
+++ src/comps/Certificates/Certificates.js
@@ -9,14 +9,19 @@ const Certificates = () => {
       {CertificatesData.map((data) => {
         return (
           <div key={nanoid()} className="certificate-box">
-            <h2 className="certificate-heading">{data.heading}</h2>
+            <div className="certificate-title-wrapper">
+              <div className="certificate-icon">{data.icon}</div>
+              <h3 className="certificate-heading">{data.heading}</h3>
+            </div>
             {data.certificates.map((certificate) => {
               return (
-              <div key={nanoid()} className="certificate">
-                <h4>{certificate.title}</h4>
-                <p>{certificate.source}</p>
-                <a href={certificate.img} target="_blank" rel="noreferrer"><img src={certificate.img} alt="" /></a>
-              </div>
+                <div key={nanoid()} className="certificate">
+                  <h4>{certificate.title}</h4>
+                  <p>{certificate.source}</p>
+                  <a href={certificate.img} target="_blank" rel="noreferrer">
+                    <img src={certificate.img} alt="" />
+                  </a>
+                </div>
               );
             })}
           </div>
